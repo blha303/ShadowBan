630e14b83e323168e29ee4481d3916f3d7e0387d
blha303 <stevensmith.ome@gmail.com>
2013-04-17 23:42:39 +0800

    Cut down on if(debug), added travis

--- src/main/java/me/blha303/shadowban/ShadowBan.java
+++ src/main/java/me/blha303/shadowban/ShadowBan.java
@@ -24,8 +24,7 @@ public class ShadowBan extends JavaPlugin implements Listener {
         saveDefaultConfig();
         debug = getConfig().getBoolean("debug");
         getServer().getPluginManager().registerEvents(this, this);
-        if (debug)
-            debug("Events registered");
+        debug("Events registered");
         playerlist = getConfig().getStringList("shadowbannedList");
     }
 
@@ -34,25 +33,21 @@ public class ShadowBan extends JavaPlugin implements Listener {
     }
 
     public void debug(String msg) {
-        this.getLogger().info("DEBUG: " + msg);
+        if (debug)
+            this.getLogger().info("DEBUG: " + msg);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(AsyncPlayerChatEvent event) {
-        if (debug)
-            debug("Entered PlayerChatEvent");
+        debug("Entered PlayerChatEvent");
         if (playerlist.contains(event.getPlayer().getName())) {
-            if (debug)
-                debug("Playerlist contains this name!");
+            debug("Playerlist contains this name!");
             event.getRecipients().clear();
-            if (debug)
-                debug("List cleared");
+            debug("List cleared");
             event.getRecipients().add(event.getPlayer());
-            if (debug)
-                debug("Recipient added");
+            debug("Recipient added");
             this.getLogger().info(event.getPlayer().getName() + " tried to say: " + event.getMessage());
-            if (debug)
-                debug("Message sent");
+            debug("Message sent");
         }
     }
 
@@ -60,24 +55,18 @@ public class ShadowBan extends JavaPlugin implements Listener {
         String prefix = ChatColor.GRAY + "[" + ChatColor.AQUA + getDescription().getName() + ChatColor.GRAY + "] ";
         if (command.getLabel().equalsIgnoreCase("shadowban")) {
             if (sender instanceof Player) {
-                if (debug)
-                    debug("Sender is player");
+                debug("Sender is player");
                 if (args.length >= 1) {
                     if (sender.hasPermission("shadowban.use")) {
-                        if (debug)
-                            debug("One or more args");
+                        debug("One or more args");
                         for (String arg : args) {
-                            if (debug)
-                                debug("Entered for loop");
+                            debug("Entered for loop");
                             if (getServer().getPlayer(arg) != null) {
-                                if (debug)
-                                    debug("Adding arg to playerlist");
+                                debug("Adding arg to playerlist");
                                 playerlist.add(getServer().getPlayer(arg).getName());
-                                if (debug)
-                                    debug("arg added");
+                                debug("arg added");
                             } else {
-                                if (debug)
-                                    debug("couldn't find player from arg");
+                                debug("couldn't find player from arg");
                                 sender.sendMessage(prefix + ChatColor.WHITE + "Could not find player \"" + ChatColor.RED + arg + ChatColor.WHITE + "\"");
                                 return true;
                             }
@@ -91,8 +80,7 @@ public class ShadowBan extends JavaPlugin implements Listener {
                         return true;
                     }
                 } else {
-                    if (debug)
-                        debug("not enough args");
+                    debug("not enough args");
                     return false;
                 }
             } else {
@@ -115,20 +103,15 @@ public class ShadowBan extends JavaPlugin implements Listener {
             }
         } else if (command.getLabel().equalsIgnoreCase("shadowbanoffline")) {
             if (sender instanceof Player) {
-                if (debug)
-                    debug("Sender is player");
+                debug("Sender is player");
                 if (args.length >= 1) {
                     if (sender.hasPermission("shadowban.offline")) {
-                        if (debug)
-                            debug("One or more args");
+                        debug("One or more args");
                         for (String arg : args) {
-                            if (debug)
-                                debug("Entered for loop");
-                            if (debug)
-                                debug("Adding arg to playerlist");
+                            debug("Entered for loop");
+                            debug("Adding arg to playerlist");
                             playerlist.add(arg);
-                            if (debug)
-                                debug("arg added");
+                            debug("arg added");
                             getConfig().set("shadowbannedList", playerlist);
                             saveConfig();
                             sender.sendMessage(prefix + ChatColor.GREEN + arg + " " + ChatColor.WHITE + "added to shadowban list");
@@ -139,8 +122,7 @@ public class ShadowBan extends JavaPlugin implements Listener {
                         return true;
                     }
                 } else {
-                    if (debug)
-                        debug("not enough args");
+                    debug("not enough args");
                     return false;
                 }
             } else {
@@ -158,20 +140,15 @@ public class ShadowBan extends JavaPlugin implements Listener {
             }
         } else if (command.getLabel().equalsIgnoreCase("shadowunban")) {
             if (sender instanceof Player) {
-                if (debug)
-                    debug("Sender is player");
+                debug("Sender is player");
                 if (args.length >= 1) {
                     if (sender.hasPermission("shadowban.unban")) {
-                        if (debug)
-                            debug("One or more args");
+                        debug("One or more args");
                         for (String arg : args) {
-                            if (debug)
-                                debug("Entered for loop");
-                            if (debug)
-                                debug("Removing arg from playerlist");
+                            debug("Entered for loop");
+                            debug("Removing arg from playerlist");
                             playerlist.remove(arg);
-                            if (debug)
-                                debug("arg removed");
+                            debug("arg removed");
                             getConfig().set("shadowbannedList", playerlist);
                             saveConfig();
                             sender.sendMessage(prefix + ChatColor.GREEN + arg + " " + ChatColor.WHITE + "removed from shadowban list");
@@ -182,8 +159,7 @@ public class ShadowBan extends JavaPlugin implements Listener {
                         return true;
                     }
                 } else {
-                    if (debug)
-                        debug("not enough args");
+                    debug("not enough args");
                     return false;
                 }
             } else {
